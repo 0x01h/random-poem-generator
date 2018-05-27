72e7bc01b0611c263dc1daa40a4fcd14a71049b6
0x4F72E7756E20D67A64656D6972 <benorcunozdemir@gmail.com>
2018-05-27 16:23:12 +0300

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,32 @@
+import random
+import json
+import urllib.request
+import urllib.parse
+
+def string():
+    contents = urllib.request.urlopen("http://poetrydb.org/title").read()
+    readable = contents.decode('utf-8')
+
+    data = json.loads(readable)
+
+    poemUrl = "http://poetrydb.org/title/" + urllib.parse.quote((data['titles'][random.randint(0,2971)]), safe=':/?*=\'')
+
+    contents = urllib.request.urlopen(poemUrl).read()
+    readable = contents.decode('utf-8')
+
+    data = json.loads(readable)
+
+    title = data[0]['title']
+    author = data[0]['author']
+    lines = data[0]['lines']
+    poem = ""
+
+    print (title)
+    print (author)
+    for i in range(0,len(lines)):
+        poem = poem + (lines[i]) + " \n"
+    print (poem)
+
+    return data
+
+string()
