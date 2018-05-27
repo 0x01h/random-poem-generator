d8f82f2860e4126412cc65c553ee177599608fd7
0x4F72E7756E20D67A64656D6972 <benorcunozdemir@gmail.com>
2018-05-27 16:26:57 +0300

    Update poem.py

--- poem.py
+++ poem.py
@@ -6,29 +6,35 @@ import urllib.request
 import urllib.parse
 
 def random_poem():
-    contents = urllib.request.urlopen("http://poetrydb.org/title").read()
-    readable = contents.decode('utf-8')
+    try:
+        contents = urllib.request.urlopen("http://poetrydb.org/title").read()
+        readable = contents.decode('utf-8')
 
-    data = json.loads(readable)
+        data = json.loads(readable)
 
-    poemUrl = "http://poetrydb.org/title/" + urllib.parse.quote((data['titles'][random.randint(0,2971)]), safe=':/?*=\'')
+        poemUrl = "http://poetrydb.org/title/" + urllib.parse.quote((data['titles'][random.randint(0,2971)]), safe=':/?*=\'')
 
-    contents = urllib.request.urlopen(poemUrl).read()
-    readable = contents.decode('utf-8')
+        contents = urllib.request.urlopen(poemUrl).read()
+        readable = contents.decode('utf-8')
 
-    data = json.loads(readable)
+        data = json.loads(readable)
 
-    title = data[0]['title']
-    author = data[0]['author']
-    lines = data[0]['lines']
-    poem = ""
+        title = data[0]['title']
+        author = data[0]['author']
+        lines = data[0]['lines']
+        poem = ""
 
-    print (title)
-    print (author)
-    for i in range(0,len(lines)):
-        poem = poem + (lines[i]) + " \n"
-    print (poem)
+        print (title)
+        print (author)
+        
+        for i in range(0,len(lines)):
+            poem = poem + (lines[i]) + " \n"
+            
+        print (poem)
 
-    return data
+        return data
+    
+    except:
+        print ("Something went wrong!")
 
 random_poem()
